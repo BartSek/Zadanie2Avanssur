99074b618911c2ba923816feaad2c77a126c5de0
b.seklecki <b.seklecki@SD-56P61Z2>
2022-06-09 13:48:44 +0200

    Add project files.

---
+++ UnitTestProject1/GoogleMapsStartPage.cs
@@ -0,0 +1,83 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TravelTimeCheck
+{
+    internal class GoogleMapsStartPage
+    {
+        private IWebDriver Driver { get; set; }
+
+        public GoogleMapsStartPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public bool IsVisible {
+            get
+            {
+                return Driver.Title.Contains("Mapy Google");
+            }
+            internal set { }
+        }
+
+        public int TravelTime {
+            get
+            {   
+                var timeText = Driver.FindElement(By.XPath("//div[contains(text(),' min')]")).GetDomProperty("innerText");
+                timeText = timeText.Remove(2);
+                return int.Parse(timeText);
+            } 
+            internal set { }
+        }
+        public double TravelDistance
+        {
+            get
+            {
+                double distance;
+                var distanceText = Driver.FindElement(By.XPath("//div[contains(text(),' km')]")).GetDomProperty("innerText");
+                distanceText = distanceText.Remove(3).Replace(',', '.');
+                double.TryParse(distanceText, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out distance);
+                return distance;
+            }
+            internal set { }
+        }
+
+        internal void GoTo()
+        {
+            Driver.Navigate().GoToUrl("https://www.google.com/maps/");
+        }
+
+        internal void FillOutDestinationAndSubmit(string destination)
+        {
+            Driver.FindElement(By.Id("searchboxinput")).SendKeys(destination);
+            Driver.FindElement(By.Id("searchboxinput")).SendKeys(Keys.Enter);
+        }
+
+        internal void SelectWalkTravel()
+        {
+            Driver.FindElement(By.XPath("//img[contains(@aria-label,'Pieszo')]")).Click();
+        }
+
+        internal void SelectBikeTravel()
+        {
+            Driver.FindElement(By.XPath("//img[contains(@aria-label,'Na rowerze')]")).Click();
+        }
+
+        internal void Directions()
+        {
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            Driver.FindElement(By.XPath("//button[@data-value='Wyznacz trasę']")).Click();
+            
+        }
+
+        internal void FillOutStartingPoint(string startingPoint)
+        {
+            //Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            Driver.FindElement(By.XPath("//input[contains(@placeholder,'Wybierz')]")).SendKeys(startingPoint);
+            //Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            Driver.FindElement(By.XPath("//input[contains(@placeholder,'Wybierz')]")).SendKeys(Keys.Enter);
+        }
+    }
+}
---
+++ UnitTestProject1/TravelTimeToOffice.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TravelTimeCheck
+{
+    [TestClass]
+    public class TravelTimeToOffice
+    {
+
+        private IWebDriver Driver { get; set; }
+
+        
+        public void TestBase(string address1, string address2, int expectedTravelTimeWalk, int expectedTravelDistanceWalk, int expectedTravelTimeBike, int expectedTravelDistanceBike)
+        {
+            Driver = GetChromeDriver();
+            var googleMapsStartPage = new GoogleMapsStartPage(Driver);
+            googleMapsStartPage.GoTo();
+            Assert.IsTrue(googleMapsStartPage.IsVisible);
+
+            googleMapsStartPage.FillOutDestinationAndSubmit(address1);
+            googleMapsStartPage.Directions();
+            googleMapsStartPage.FillOutStartingPoint(address2);
+            googleMapsStartPage.SelectWalkTravel();
+            Assert.IsTrue(googleMapsStartPage.TravelTime < expectedTravelTimeWalk);
+            Assert.IsTrue(googleMapsStartPage.TravelDistance < expectedTravelDistanceWalk);
+            googleMapsStartPage.SelectBikeTravel();
+            Assert.IsTrue(googleMapsStartPage.TravelTime < expectedTravelTimeBike);
+            Assert.IsTrue(googleMapsStartPage.TravelDistance < expectedTravelDistanceBike);
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestBase("Chłodna 51, Warszawa", "Plac Defilad 1, Warszawa", 40, 3, 15, 3);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            TestBase("Plac Defilad 1, Warszawa", "Chłodna 51, Warszawa", 40, 3, 15, 3);
+
+        }
+
+        private IWebDriver GetChromeDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outPutDirectory);
+        }
+    }
+}
